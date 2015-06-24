7fec35e6e63a4acda4582759c97995ddc36c26cd
Ian Kilgore <iank@iank.org>
2015-06-24 00:51:53 -0400

    Compute 'usefulness' of receptors

--- gen_receptors.py
+++ gen_receptors.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import division
 import cv2
 import cv2.cv as cv
 import sys
@@ -8,6 +9,17 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
+
+# useful in info theory to let 0log0 = 0
+def _ilog(x):
+    if (x==0):
+        return 0
+    return np.log2(x)
+
+
+# Binary entropy function
+def _h_(x):
+    return -1*(x*_ilog(x) + (1-x)*_ilog(1-x))
 
 def _receptor_endpoints(img, receptor):
     x0, y0 = _get_centroid(img)
@@ -59,10 +71,64 @@ def _draw_receptors(img, receptors):
 
     for receptor in receptors:
         pt1, pt2 = _receptor_endpoints(img, receptor)
-#        color = (0, (_p_activated(img, receptor) - min(activations)) / (max(activations) - min(activations)) * 255, 0)
         color = (0, _p_activated(img, receptor), 0)
         cv2.line(img_draw, pt1, pt2, color, 1)
     return img_draw
+
+def compute_usefulness(images, receptor):
+    num_letters = sum([len(images[x]) for x in images.keys()])
+    #print(num_letters)
+
+    # Computer p(X=x) prior
+    px = {}
+    for letter in images.keys():
+        px[letter] = len(images[letter]) / num_letters
+    #print("P(X)")
+    #print(px)
+
+    p1_x = {}
+    py = 0
+    for letter in images.keys():
+        p1_x[letter] = 0
+        for instance in images[letter]:
+            if (_p_activated(instance, receptor)):
+                p1_x[letter] += 1
+                py += 1
+        p1_x[letter] /= len(images[letter])
+    py /= num_letters
+
+    if (py == 0):
+        return 0 # receptor is not useful if it is -never- on. HYX high, HXY 0
+
+    #print("P(Y): {0}".format(py))
+    #print("P(1|X=x)")
+    #print(p1_x)
+    
+    HYX = 0
+    # Find H(Y|X) = average of H(Y|X=x) = h(p(Y=1|X=x))
+    for letter in images.keys():
+        HYX += _h_(p1_x[letter])
+    HYX /= len(images.keys())
+    #print("H(Y|X): {0}".format(HYX))
+
+    px_1 = {}
+    for letter in images.keys():
+        # Find p(X=x|Y=1) = p(Y=1|X=x) * p(X=x) / P(Y=y)  -- (Bayes)
+        px_1[letter] = p1_x[letter] * px[letter] / py
+    #print("P(X=x|Y=1)")
+    #print(px_1)
+
+    HXY = 0
+    # Find H(X|Y=1) = -1*sum_x(p(X=x|Y=1))
+    for letter in images.keys():
+        HXY += px_1[letter]*_ilog(px_1[letter])
+    HXY *= -1
+    #print("H(X|Y): {0}".format(HXY))
+
+    # HXY: How well receptor splits letter space (maximize this)
+    # HYX: consistent receptors across variants of symbol minimize this
+    usefulness = HXY * (1 - HYX)
+    return usefulness
 
 def load_images(im_directory, label_filename):
     images = {}
@@ -109,17 +175,35 @@ def gen_receptors(n):
 # where labels is "filename,label" each line
 
 images = load_images(sys.argv[1], sys.argv[2])
-print_frequency_table(images)
-receptors = gen_receptors(300)
+#print_frequency_table(images)
+receptors = gen_receptors(4)
 
-img = images['f'][0]
+usefulness = [0]*len(receptors)
+for k,receptor in enumerate(receptors):
+    us = compute_usefulness(images, receptor)
+    print("Receptor {0} useful {1}".format(k,us))
+    usefulness[k] = us
 
-img_draw = _draw_receptors(img, receptors)
-plt.imshow(img_draw, interpolation='bicubic')
-plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-plt.savefig('/var/www/vara/rec.png', bbox_inches='tight')
+receptor_field = np.zeros((len(receptors), 5))
+for k,receptor in enumerate(receptors):
+    receptor_field[k][0] = receptor['center'][0]
+    receptor_field[k][1] = receptor['center'][1]
+    receptor_field[k][2] = receptor['length']
+    receptor_field[k][3] = receptor['angle']
+    receptor_field[k][4] = usefulness[k]
 
-# TODO: compute receptor activation for all images
-# TODO: compute entropies for receptors
+receptor_field = receptor_field[receptor_field[:,4].argsort()[::-1]]
+np.save('receptor_field.npy', receptor_field)
+    
+#img = images['f'][0]
+
+#img_draw = _draw_receptors(img, receptors)
+#plt.imshow(img_draw, interpolation='bicubic')
+#plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
+#plt.savefig('/var/www/vara/rec.png', bbox_inches='tight')
+
 # TODO: prune receptor field
-# TODO: save receptor field
+# TODO: script to load receptor field and display it on arbitrary image
+# TODO: continuous generalization
+# TODO: generate training data for model: load receptor field, compute
+# activations for all images & associate w/ class label
