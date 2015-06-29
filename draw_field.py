ef368abab78d8cff4bbdcf14d5014f4a69491613
Ian Kilgore <iank@iank.org>
2015-06-29 15:33:12 -0400

    draw field for specific receptors

--- draw_field.py
+++ draw_field.py
@@ -3,12 +3,13 @@ import cv2
 import receptor_common as rc
 import sys
 
-# Usage: draw_field.py field.npy image.png N output.png
-# Draw N most useful receptors from field.npy on image.png
+# Usage: draw_field.py field.npy image.png output.png [idx1 [idx2 [...]]]
+# Draw selected receptors from field.npy on image.png
 receptors = rc.load_field(sys.argv[1])
 
 desc = rc.load_image(sys.argv[2])
+filename = sys.argv[3]
+idxs = [int(x) for x in sys.argv[4:]]
+img = rc.draw_receptors_activated(desc, [receptors[i] for i in idxs])
 
-N = int(sys.argv[3])
-img = rc.draw_receptors_activated(desc, receptors[:N])
-rc.save_plot(img, sys.argv[4])
+rc.save_plot(img, filename)
